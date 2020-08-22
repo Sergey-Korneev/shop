b4c97ffdd91e6d602902d2bd85931b887feb6257
Sergey-Korneev <sak24102001@yandex.ru>
2020-08-22 17:54:02 +0300

    create

---
+++ src/data/products.js
@@ -0,0 +1,53 @@
+export default [
+  {
+    name: 'ПК Acer Aspire XC-330',
+    price: '10 999',
+    img: './img/PC_Acer_Aspire_XC-330.jpg',
+  },
+  {
+    name: '18.5" Монитор HP 19ka',
+    price: '4 399',
+    img: './img/moni_HP_19ka.jpg',
+  },
+  {
+    name: '14.1" Нетбук Prestigio Smartbook 141S синий',
+    price: '14 999',
+    img: './img/Lenovo_Ideapad_S145.jpg',
+  },
+  {
+    name: '10.8" Планшет Huawei MediaPad M6 10 64 ГБ',
+    price: '24 999',
+    img: './img/Huawei_MediaPad_M6.jpg',
+  },
+  {
+    name: '6.5" Смартфон Samsung Galaxy A51 64 ГБ',
+    price: '16 999',
+    img: './img/Samsung_Galaxy_A51.jpg',
+  },
+  {
+    name: '5.8" Смартфон Apple iPhone 11 Pro',
+    price: '79 999',
+    img: './img/Apple_iPhone_11.jpg',
+  },
+  {
+    name: '6.39" Смартфон Honor 9C 64 ГБ',
+    price: '11 999',
+    img: './img/Honor_9C.jpg',
+  },
+  {
+    name: '6.21" Смартфон Honor 10i 128 ГБ',
+    price: '14 999',
+    img: './img/Honor_10i.jpg',
+  },
+  {
+    name: '6.53" Смартфон Xiaomi Redmi Note 8 Pro 128 ГБ',
+    price: '18 999',
+    img: './img/Xiaomi_Redmi_Note.jpg',
+  },
+  {
+    name: '15.6" Ноутбук Lenovo Ideapad S145-15AST',
+    price: '22 999',
+    img: './img/Prestigio_Smartbook_141S.jpg',
+  },
+
+];
